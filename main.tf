585e345eea0bbcb36d088d5e8d4e0de26ae39ba0
m.mahapatra <m.mahapatra@salesforce.com>
2024-11-08 15:39:45 +0530

    update library template

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"  # Replace with your desired region
+  region = var.region  # Replace with your desired region
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "mithun-tf-aditya-bucket"       #Simple bucket creation for demo purpose
+  bucket = var.bucket_name     #Simple bucket creation for demo purpose
 
   tags = {
     Name        = "My bucket"
